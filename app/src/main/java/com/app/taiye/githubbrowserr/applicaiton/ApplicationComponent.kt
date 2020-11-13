5423c1418e21c8909585c91434dfe003d5a8c3de
Taiye Emmanuel <taiyeo@remitly.com>
2020-11-13 11:19:03 +0100

    Create App repository Add AppRepository.kt to AppComponent

--- app/src/main/java/com/app/taiye/githubbrowserr/applicaiton/ApplicationComponent.kt
+++ app/src/main/java/com/app/taiye/githubbrowserr/applicaiton/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.app.taiye.githubbrowserr.applicaiton
 
 import android.content.Context
 import com.app.taiye.githubbrowserr.githubapi.GithubApiModule
+import com.app.taiye.githubbrowserr.repository.AppRepository
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -10,6 +11,8 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [GithubApiModule::class])
 interface ApplicationComponent {
+
+    fun  appRepository():AppRepository
 
     @Component.Factory
     interface  Factory{
---
+++ repository/src/main/java/com/app/taiye/githubbrowserr/repository/AppRepository.kt
@@ -0,0 +1,15 @@
+package com.app.taiye.githubbrowserr.repository
+
+import com.app.taiye.githubbrowserr.githubapi.GitHubApi
+import com.app.taiye.githubbrowserr.githubapi.model.RepoApiModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+class AppRepository @Inject constructor(private val gitHubApi: GitHubApi) {
+
+    fun getTopRepos():List<RepoApiModel>{
+       return gitHubApi.getRepositories()
+    }
+}
